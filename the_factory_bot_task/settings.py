986184d5233aaaddd3494961d3648a16e3bff2de
Ahatron <ahatpolatov@gmail.com>
2023-08-16 10:41:28 +0600

    settings.py

--- the_factory_bot_task/settings.py
+++ the_factory_bot_task/settings.py
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "ru-ru"
+LANGUAGE_CODE = "en-us"
 LOCALE_PATHS = [BASE_DIR / "translations/locale"]
 
 TIME_ZONE = "UTC"
