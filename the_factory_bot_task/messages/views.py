5ae31b213292197abb348983ce8d172365e94ac4
Ahatron <ahatpolatov@gmail.com>
2023-08-16 10:30:15 +0600

    Можно теперь получить собственный токен.

--- the_factory_bot_task/messages/views.py
+++ the_factory_bot_task/messages/views.py
@@ -1,7 +1,10 @@
 import asyncio
 
-from rest_framework import viewsets, permissions
-from rest_framework.exceptions import APIException, NotFound
+from rest_framework import viewsets, permissions, status
+from rest_framework.exceptions import APIException
+from rest_framework.decorators import action
+from rest_framework.request import Request
+from rest_framework.response import Response
 from aiogram import Bot
 from django.conf import settings
 from django.utils.translation import gettext as _
@@ -26,7 +29,7 @@ class MessageViewSet(viewsets.ModelViewSet):
         except aiogram.utils.exceptions.ValidationError:
             raise APIException(
                 _("The bot token was not specified."),
-                400,
+                status.HTTP_400_BAD_REQUEST,
             )
         token = None
         try:
@@ -34,12 +37,12 @@ class MessageViewSet(viewsets.ModelViewSet):
         except models.Token.DoesNotExist:
             raise APIException(
                 _("You don't have a token."),
-                400,
+                status.HTTP_400_BAD_REQUEST,
             )
         if token.chat_id is None:
             raise APIException(
                 _("You haven't linked the token to the bot."),
-                400,
+                status.HTTP_400_BAD_REQUEST,
             )
         asyncio.run(
             bot.send_message(
@@ -54,10 +57,22 @@ class TokenViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated]
     queryset = models.Token.objects.all()
 
+    @action(detail=False)
+    def my_token(self, request: Request) -> Response:
+        try:
+            token = models.Token.objects.get(user=self.request.user)
+        except models.Token.DoesNotExist:
+            raise APIException(_("You don't have a token."), status.HTTP_404_NOT_FOUND)
+        else:
+            serailizer = serializers.TokenSerializer(token)
+            return Response(serailizer.data)
+
     def perform_create(self, serializer):
         try:
             models.Token.objects.get(user=self.request.user)
         except models.Token.DoesNotExist:
             serializer.save(user=self.request.user)
         else:
-            raise APIException(_("You have already created a token."), 400)
+            raise APIException(
+                _("You have already created a token."), status.HTTP_400_BAD_REQUEST
+            )
